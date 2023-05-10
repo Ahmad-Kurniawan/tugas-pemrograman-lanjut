30ee835fd34ff5c840142dff10d2c1c290c8226e
Ahmad-Kurniawan <133091979+Ahmad-Kurniawan@users.noreply.github.com>
2023-05-10 16:29:39 +0700

    Add files via upload

---
+++ Latihan 1/CobaTayo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class CobaTayo {
+    public static void main(String[] args) {
+        Tayo tayo = new Tayo();
+        tayo.a("Tayo");
+        tayo.a("Biru", 6, "BM 1234 TIF");
+        tayo.a(60, 2);
+    }
+}
