4febb3a32f4320d226feaa2b843f88354c42e3e0
Ahmad-Kurniawan <133091979+Ahmad-Kurniawan@users.noreply.github.com>
2023-05-10 16:37:48 +0700

    Update Mobil.java
    ydshy7sdf

--- Mobil.java
+++ Mobil.java
@@ -2,12 +2,7 @@
 class Mobil {
     String merek, warna, harga;
     int tahun;
-    Mobil(){
-    }
-    Mobil(String merek, String warna, String harga){
-    }
-    Mobil(int tahun){
-    }
+
 
     void setInfoMobil(String merek, String warna, int tahun){
         System.out.println("Merek Mobil \t: "+merek+"\nWarna Mobil \t: "+warna+"\nTahun Produksi \t: "+tahun);
